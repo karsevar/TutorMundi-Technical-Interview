cffbf75397057dce0970069138e062650daf513a
masonkarsevar <masonkarsevar@gmail.com>
2020-02-08 08:07:51 -0800

    HeaderTutor component basic structure. No styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import HeaderTutor from "./components/HeaderTutor";
+
+function App() {
+  return (
+    <div className='App'>
+      <HeaderTutor />
+    </div>
+  );
+}
+
+export default App;
